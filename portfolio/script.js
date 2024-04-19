12e2659c3bc4e1c9334a625ac7df049526bbd055
Mrsanjayshah <167520141+Mrsanjayshah@users.noreply.github.com>
2024-04-19 15:36:33 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+const btn12 = document.querySelector('.btn12');
+
+btn12.addEventListener('click', () => {
+    btn12.style.backgroundColor = '#ffac09';
+    btn12.style.boxShadow = '0 0 40px #ffac09';
+    btn12.textContent="Submit Successful";
+
+});
+
